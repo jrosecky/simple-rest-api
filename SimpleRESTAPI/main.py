a933f0f07912fb6dfa85a38d085576649e4a0a60
jrosecky <jrosecky@proton.me>
2026-04-19 20:39:45 +0200

    Fix workflow lint issues and update item endpoint

--- SimpleRESTAPI/main.py
+++ SimpleRESTAPI/main.py
@@ -1,21 +1,25 @@
 from fastapi import FastAPI
-from models.item import Item
 from fastapi import HTTPException
 
-# crates the API app itself
+from models.item import Item
+
+# creates the API app itself
 app = FastAPI(title="My API")
 
 # "database" (temporary in-memory storage)
 items = []
+
 
 # root endpoint
 @app.get("/")
 def root():
     return {"message": "API is running"}
 
+
 @app.get("/items")
 def get_items():
     return items
+
 
 @app.post("/items")
 def add_item(item: Item):
@@ -23,19 +27,21 @@ def add_item(item: Item):
     items.append(item_data)
     return {"message": "Item added", "item": item_data}
 
+
 @app.delete("/items/{item_id}")
 def delete_item(item_id: int):
     if 0 <= item_id < len(items):
         deleted = items.pop(item_id)
         return {"message": "Item deleted", "item": deleted}
-    else:
-        raise HTTPException(status_code=404, detail="Item not found")
+
+    raise HTTPException(status_code=404, detail="Item not found")
+
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     if 0 <= item_id < len(items):
         updated_item = item.model_dump()
-        items[item_id] = update_item
-        return{"message": "Item updated", "item": updated_item}
-    else:
-        raise HTTPException(status_code=404, detail="Item not found")
+        items[item_id] = updated_item
+        return {"message": "Item updated", "item": updated_item}
+
+    raise HTTPException(status_code=404, detail="Item not found")
